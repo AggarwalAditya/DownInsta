7d376206afedf56d70f8dddd09e36c8de74c3573
Aditya <adi.adag@gmail.com>
2016-10-15 15:00:26 +0530

    About_Activity layout changed

--- app/src/main/java/com/yellowsoft/subhankar/downinsta/AboutActivity.java
+++ app/src/main/java/com/yellowsoft/subhankar/downinsta/AboutActivity.java
@@ -1,18 +1,37 @@
 package com.yellowsoft.subhankar.downinsta;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
+
 
 public class AboutActivity extends AppCompatActivity {
 
+    private RelativeLayout rv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        try {
+            Glide.with(this).load(R.drawable.cover).into((ImageView) findViewById(R.id.backdrop));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -21,9 +40,17 @@ public class AboutActivity extends AppCompatActivity {
 
         if (id == R.id.home) {
             // Respond to the action bar's Up/Home button
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void linktorep(View view)
+    {
+        Uri uri = Uri.parse("https://github.com/raj-subhankar/DownInsta"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
 }
+
